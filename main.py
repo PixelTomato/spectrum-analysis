65fdeaf851ac1d684e6527937e2b52f1bcde64a0
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-08-31 16:19:23 -0700

    consolidated spectrogram options to tool window

--- main.py
+++ main.py
@@ -26,8 +26,6 @@ def capture_routine(sample_rate, sample_count):
     global capture_data
     global capture_flag
 
-    print(f"Started capture @ {sample_rate} Hz, {sample_count}/s")
-
     HANN_WINDOW = np.hanning(sample_count)
 
     # device = sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True)
@@ -42,8 +40,6 @@ def capture_routine(sample_rate, sample_count):
 
             capture_data = fft_data
             capture_flag += 1
-
-    print("Stopping capture")
 
     capture_stop.clear()
 
@@ -86,44 +82,50 @@ dpg.create_viewport(title="Spectrum Analysis", width=WIDTH, height=HEIGHT, vsync
 with dpg.texture_registry():
     dpg.add_dynamic_texture(COL_COUNT, ROW_COUNT, heatmap_data, tag="heatmap_texture")
 
-with dpg.window(label="Options", width=250, height=HEIGHT, no_close=True, no_move=True):
-    dpg.add_slider_float(
-        label="Max Hz",
-        default_value=(SAMPLE_RATE / 2),
-        min_value=100.0,
-        max_value=(SAMPLE_RATE / 2),
-        callback=slider_callback,
-        tag="heatmap_crop_slider",
-    )
-
-    dpg.add_slider_float(
-        label="Multiplier",
-        default_value=1.0,
-        min_value=0.1,
-        max_value=10.0,
-        callback=slider_callback,
-        tag="amplifier_slider",
-    )
-
-    dpg.add_checkbox(
-        label="Log Filter",
-        default_value=True,
-        callback=checkbox_callback,
-        tag="log_filter_checkbox",
-    )
-
+with dpg.window(label="Options", width=250, height=HEIGHT, no_close=True, no_move=True, no_collapse=True):
     dpg.add_button(
         label="Restart Capture",
         callback=start_capture,
     )
 
-with dpg.window(label="Spectrogram", pos=(250, 0), width=480, height=360, no_close=True):  # noqa: SIM117
-    with dpg.plot(width=-1, height=-20, tag="spectrogram_plot"):
+with dpg.window(label="Spectrogram", pos=(260, 10), width=720, height=480, no_close=True, no_scrollbar=True):
+    with dpg.child_window(border=True, width=200, height=-1):
+        dpg.add_slider_float(
+            label="Max Hz",
+            default_value=(SAMPLE_RATE / 2),
+            min_value=100.0,
+            max_value=(SAMPLE_RATE / 2),
+            callback=slider_callback,
+            width=-50,
+            tag="heatmap_crop_slider",
+        )
+
+        dpg.add_slider_float(
+            label="Amp",
+            default_value=1.0,
+            min_value=0.1,
+            max_value=10.0,
+            callback=slider_callback,
+            width=-50,
+            tag="amplifier_slider",
+        )
+
+        dpg.add_checkbox(
+            label="Log Filter",
+            default_value=True,
+            callback=checkbox_callback,
+            tag="log_filter_checkbox",
+        )
+
+    with dpg.plot(pos=(216, 27), width=-1, height=-1, tag="spectrogram_plot"):
         dpg.add_plot_axis(dpg.mvXAxis, label="Frequency (Hz)", tag="x_axis")
         dpg.add_plot_axis(dpg.mvYAxis, label="Time (Frames)", tag="y_axis")
 
         dpg.add_image_series(
-            texture_tag="heatmap_texture", bounds_min=(0, 0), bounds_max=(SAMPLE_RATE / 2, ROW_COUNT), parent="x_axis"
+            texture_tag="heatmap_texture",
+            bounds_min=(0, 0),
+            bounds_max=(SAMPLE_RATE / 2, ROW_COUNT),
+            parent="x_axis",
         )
 
 dpg.setup_dearpygui()
@@ -147,11 +149,9 @@ while dpg.is_dearpygui_running():
         v *= MULTIPLIER
 
         heatmap_colors = np.flipud(heatmap_data.reshape(ROW_COUNT, COL_COUNT, 4))
-        heatmap_colors[:, :, 0] = np.clip((-0.42 * v**3 + 1.25 * v**2 + 0.17 * v + 0.02), 0.0, 1.0)
-        heatmap_colors[:, :, 1] = np.clip((1.83 * v**3 - 1.27 * v**2 + 0.44 * v), 0.0, 1.0)
-        heatmap_colors[:, :, 2] = np.clip(
-            (-2.85 * v**3 + 5.16 * v**2 - 1.63 * v + 0.3 - 0.25 * np.exp(-120 * v)), 0.0, 1.0
-        )
+        heatmap_colors[:, :, 0] = v * 0.1
+        heatmap_colors[:, :, 1] = v * 0.5
+        heatmap_colors[:, :, 2] = v * 1.0
         heatmap_colors[:, :, 3] = 1.0
 
         dpg.set_value("heatmap_texture", heatmap_data)
