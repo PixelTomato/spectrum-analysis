c1e247ed3fc37b93cf7212b3da7a54e5f299bff2
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-08-30 19:02:55 -0700

    migrated from pygame to dearpygui

--- main.py
+++ main.py
@@ -1,78 +1,53 @@
+import dearpygui.dearpygui as dpg
 import numpy as np
-import pygame as pg
 import soundcard as sc
 
-pg.init()
-
-WIDTH, HEIGHT = 1044, 512
+WIDTH = 1000
+HEIGHT = 600
 
 FREQUENCY = 48000
 SAMPLES = 2048
 
 HANN_WINDOW = np.hanning(SAMPLES)
 
-screen = pg.display.set_mode((WIDTH, HEIGHT), pg.RESIZABLE, vsync=1)
-pg.display.set_caption("Spectrum Analysis")
+ROW_COUNT = 256
+COL_COUNT = SAMPLES // 2 + 1
 
-history = np.ndarray((SAMPLES // 2 + 1, 1024))
+last_input = np.zeros(COL_COUNT, dtype=np.float32)
 
-clock = pg.time.Clock()
+heatmap_texture = np.zeros(COL_COUNT * ROW_COUNT * 4, dtype=np.float32)
+
+dpg.create_context()
+dpg.show_metrics()
+dpg.create_viewport(title="Spectrum Analysis", width=WIDTH, height=HEIGHT, vsync=True)
+
+with dpg.texture_registry():
+    dpg.add_dynamic_texture(COL_COUNT, ROW_COUNT, heatmap_texture, tag="heatmap")
+
+with dpg.window(label="Options", width=250, height=HEIGHT, no_close=True, no_move=True):
+    dpg.add_text("Testing...")
+
+with dpg.window(label="Spectrogram", pos=(250, 0), width=750, height=600, no_close=True, no_resize=True):
+    dpg.add_image("heatmap", width=734, height=565)
+
+dpg.setup_dearpygui()
+dpg.show_viewport()
 
 device = sc.get_microphone(id=str(sc.default_speaker().name), include_loopback=True)
 
-
-def draw_heatmap(x, y, width, height):
-    heatmap = pg.Surface((SAMPLES // 2, height))
-
-    pixels = pg.surfarray.pixels3d(heatmap)
-    pixels[:, :, 1] = history[: SAMPLES // 2, :height]
-    del pixels
-
-    screen.blit(pg.transform.smoothscale(heatmap, (width, height)), (x, y))
-
-    pg.draw.rect(screen, (120, 120, 120), (x, y, width, height), 2)
-
-
-def draw_visualizer(x, y, width, height):
-    bars = pg.Surface((SAMPLES // 2, 100))
-
-    for i, amp in enumerate(history[:, 0] / 4):
-        pg.draw.rect(bars, (0, 255, 0), (i, y + 100 - amp, 1, amp))
-
-    screen.blit(pg.transform.smoothscale(bars, (width, height)), (x, y))
-
-    pg.draw.rect(screen, (120, 120, 120), (x, y, width, height), 2)
-
-
 with device.recorder(FREQUENCY, 1, SAMPLES) as stream:
-    running = True
-    while running:
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                running = False
-            elif event.type == pg.VIDEORESIZE:
-                WIDTH, HEIGHT = event.w, event.h
-
+    while dpg.is_dearpygui_running():
         data = stream.record(SAMPLES).flatten() * HANN_WINDOW
 
         amp = np.abs(np.fft.fft(data)[: SAMPLES // 2 + 1])
-        amp = 20 * np.log10(amp + 0.000001)
+        amp = np.log10(amp + 1e-6)
 
-        row = 2 * amp / SAMPLES * 7500
-        row[0] /= 2
-        row[-1] /= 2
-        row = np.clip(row, 0, 255)
+        history = heatmap_texture.reshape(ROW_COUNT, COL_COUNT, 4)
+        history[1:] = history[:-1]
+        history[0, :, 1] = amp
+        history[0, :, 3] = 1.0
+        dpg.set_value("heatmap", heatmap_texture)
 
-        history[:, 1:] = history[:, :-1]
-        history[:, 0] = row
+        dpg.render_dearpygui_frame()
 
-        screen.fill((0, 0, 0))
-
-        draw_visualizer(10, 10, WIDTH - 20, 70)
-        draw_heatmap(10, 90, WIDTH - 20, HEIGHT - 100)
-
-        pg.display.flip()
-
-        clock.tick()
-
-pg.quit()
+dpg.destroy_context()
